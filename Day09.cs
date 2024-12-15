ff5e226bad0231d701eef0a109b1c47221531b0a
Henry <henry50@users.noreply.github.com>
2024-12-15 22:47:22 +0000

    Day 9 complete

--- Day09.cs
+++ Day09.cs
@@ -1,16 +1,114 @@
-﻿
+﻿using System.Linq;
+
 namespace AdventOfCode2024
 {
     internal class Day09 : Solution
     {
         public override string Part1(string input)
         {
-            return "Not implemented";
-        }
-        public override string Part2(string input)
-        {
-            return "Not implemented";
+            int[] blocks = input.TrimEnd().Select(c => c - '0').ToArray();
+            long total = 0;
+            int pos = 0;
+            int end = blocks.Length - 1;
+            for (int i = 0; i < blocks.Length; i++)
+            {
+                // even index - file
+                if (i % 2 == 0)
+                {
+                    int fileId = i / 2;
+                    for (int j = 0; j < blocks[i]; j++)
+                    {
+                        total += ((pos++) * fileId);
+                    }
+                }
+                // odd index - free space
+                else
+                {
+                    // if end is less than i all remaining space is empty
+                    if (end < i)
+                    {
+                        return total.ToString();
+                    }
+                    for (int j = 0; j < blocks[i]; j++)
+                    {
+                        // get the file id of the rightmost file
+                        int fileId = end / 2;
+                        // add it to the total
+                        total += ((pos++) * fileId);
+
+                        // decrement it's size
+                        blocks[end]--;
+                        // if it's now empty, move to the next rightmost file
+                        if (blocks[end] == 0)
+                        {
+                            end -= 2;
+                        }
+                    }
+                }
+            }
+            return total.ToString();
         }
 
+        public override string Part2(string input)
+        {
+            int[] blocks = input.TrimEnd().Select(c => c - '0').ToArray();
+            long total = 0;
+            int pos = 0;
+            int end = blocks.Length - 1;
+            for (int i = 0; i < blocks.Length; i++)
+            {
+                // even index - file
+                if (i % 2 == 0)
+                {
+                    // negative size indicates moved file, skip it's position
+                    if (blocks[i] < 0)
+                    {
+                        // blocks[i] is -1 * the amount to skip, so subtract it from pos
+                        pos -= blocks[i];
+                    }
+                    else
+                    {
+                        int fileId = i / 2;
+                        for (int j = 0; j < blocks[i]; j++)
+                        {
+                            total += ((pos++) * fileId);
+                        }
+                    }
+                }
+                // odd index - free space
+                else
+                {
+                    // try and move every file from the end backwards into this empty space
+                    // until it's full or nothing fits
+                    ref int sizeOfEmptySpace = ref blocks[i];
+                    for (int j = end; j > i; j -= 2)
+                    {
+                        ref int sizeOfFileToMove = ref blocks[j];
+                        if (sizeOfFileToMove > 0 && sizeOfEmptySpace >= sizeOfFileToMove)
+                        {
+                            int fileId = j / 2;
+
+                            // move file
+                            for (int k = 0; k < sizeOfFileToMove; k++)
+                            {
+                                total += ((pos++) * fileId);
+                            }
+                            // remove the size of the moved file from the free space
+                            sizeOfEmptySpace -= sizeOfFileToMove;
+                            // store the negated size of the moved file in it's old location so pos can be skipped later
+                            sizeOfFileToMove = -sizeOfFileToMove;
+                            // if this space is now full, break
+                            if (sizeOfEmptySpace == 0)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    // increment position past unfillable empty space
+                    pos += sizeOfEmptySpace;
+                }
+            }
+            return total.ToString();
+        }
     }
 }

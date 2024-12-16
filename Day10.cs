9aa47fae27b41001d2e87dd3dfc5f398fec30959
Henry <henry50@users.noreply.github.com>
2024-12-16 23:20:16 +0000

    Day 10 complete, added Grid util class

--- Day10.cs
+++ Day10.cs
@@ -1,16 +1,65 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode2024.Util;
+
 namespace AdventOfCode2024
 {
     internal class Day10 : Solution
     {
         public override string Part1(string input)
         {
-            return "Not implemented";
-        }
-        public override string Part2(string input)
-        {
-            return "Not implemented";
+            return Solve(input, false);
         }
 
+        public override string Part2(string input)
+        {
+            return Solve(input, true);
+        }
+
+        private static string Solve(string input, bool distinctPaths)
+        {
+            int trails = 0;
+            Grid<int> grid = new(input, c => (int)char.GetNumericValue(c));
+            var trailheads = grid.FindAll(0);
+            foreach (GridCell<int> trailhead in trailheads)
+            {
+                Queue<GridCell<int>> frontier = [];
+                HashSet<GridCell<int>> ends = [];
+                frontier.Enqueue(trailhead);
+                while (frontier.Count > 0)
+                {
+                    var current = frontier.Dequeue();
+                    var neighbours = current
+                        .GetNeighbours(false)
+                        .Where(x => x.Value == current.Value + 1)
+                        .ToLookup(x => x.Value == 9);
+                    if (distinctPaths)
+                    {
+                        // include every path that reaches the ends
+                        trails += neighbours[true].Count();
+                    }
+                    else
+                    {
+                        // only include each end point once
+                        foreach (var end in neighbours[true])
+                        {
+                            ends.Add(end);
+                        }
+                    }
+                    // expand the frontier
+                    foreach (var item in neighbours[false])
+                    {
+                        if (distinctPaths || !frontier.Contains(item))
+                        {
+                            frontier.Enqueue(item);
+                        }
+                    }
+                }
+                trails += ends.Count;
+            }
+
+            return trails.ToString();
+        }
     }
 }
---
+++ Util/Grid.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2024.Util
+{
+    internal class Grid<T>
+    {
+        private char[][] CharCells { get; }
+        public GridCell<T>[][] Cells { get; }
+        private int Rowmax { get; }
+        private int Colmax { get; }
+
+        public Grid(string grid, Func<char, T> parser)
+        {
+            string[] rows = grid.Split(
+                '\n',
+                StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries
+            );
+            Rowmax = rows.Length;
+            Colmax = rows[0].Length;
+            CharCells = rows.Select(x => x.ToCharArray()).ToArray();
+            Cells = CharCells
+                .Select(
+                    (x, r) => x.Select((y, c) => new GridCell<T>(this, r, c, parser(y))).ToArray()
+                )
+                .ToArray();
+        }
+
+        public Grid(string grid)
+            : this(grid, x => (T)(object)x) { }
+
+        public bool InGridRange(int row, int col)
+        {
+            return row >= 0 && row < Rowmax && col >= 0 && col < Colmax;
+        }
+
+        public bool InGridRange((int row, int col) loc)
+        {
+            return InGridRange(loc.row, loc.col);
+        }
+
+        public GridCell<T>? Find(T value)
+        {
+            for (int r = 0; r < Rowmax; r++)
+            {
+                for (int c = 0; c < Colmax; c++)
+                {
+                    if (Cells[r][c].Value!.Equals(value))
+                    {
+                        return Cells[r][c];
+                    }
+                }
+            }
+            return null;
+        }
+
+        public IEnumerable<GridCell<T>> FindAll(T value)
+        {
+            for (int r = 0; r < Rowmax; r++)
+            {
+                for (int c = 0; c < Colmax; c++)
+                {
+                    if (Cells[r][c].Value!.Equals(value))
+                    {
+                        yield return Cells[r][c];
+                    }
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join(
+                '\n',
+                Cells.Select(x => string.Join(" | ", x.Select(y => y.ToString())))
+            );
+        }
+    }
+
+    internal class GridCell<T>(Grid<T> grid, int row, int col, T value)
+    {
+        private Grid<T> grid = grid;
+        public int Row { get; } = row;
+        public int Col { get; } = col;
+        public T Value { get; } = value;
+
+        public IEnumerable<GridCell<T>> GetNeighbours(bool includeDiagonals)
+        {
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (
+                        grid.InGridRange(Row + i, Col + j) && (i == 0 || j == 0 || includeDiagonals)
+                    )
+                    {
+                        // InGridRange check ensures non-null
+                        yield return grid.Cells[Row + i][Col + j];
+                    }
+                }
+            }
+            yield break;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("GridCell(row={0},col={1},value={2})", Row, Col, Value);
+        }
+    }
+}

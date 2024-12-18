e79ef17caa2afd94ae21ed53f9b5aae94bfdf9c9
Henry <henry50@users.noreply.github.com>
2024-12-18 00:14:21 +0000

    Day 11 complete

--- Day11.cs
+++ Day11.cs
@@ -1,16 +1,72 @@
-﻿
+﻿using System.Collections.Generic;
+using System.Linq;
+
 namespace AdventOfCode2024
 {
-    internal class Day11 : Solution
+    internal class Day11(string input) : Solution(input)
     {
-        public override string Part1(string input)
+        readonly Dictionary<(long stone, int depth), long> CountCache = [];
+        readonly List<long> stones = input.Split(' ').Select(long.Parse).ToList();
+
+        public override string Part1()
         {
-            return "Not implemented";
-        }
-        public override string Part2(string input)
-        {
-            return "Not implemented";
+            CountCache.Clear();
+            return Solve(25);
         }
 
+        public override string Part2()
+        {
+            CountCache.Clear();
+            return Solve(75);
+        }
+
+        private string Solve(int depth)
+        {
+            long result = 0;
+            foreach (long stone in stones)
+            {
+                result += CountStones(stone, 0, depth);
+            }
+            return result.ToString();
+        }
+
+        private long CountStones(long stone, int depth, int maxDepth)
+        {
+            if (depth == maxDepth)
+            {
+                return 1;
+            }
+            if (CountCache.TryGetValue((stone, depth), out var lookup))
+            {
+                return lookup;
+            }
+            if (stone == 0)
+            {
+                long result = CountStones(1, depth + 1, maxDepth);
+                CountCache.Add((stone, depth), result);
+                return result;
+            }
+
+            string stoneStr = stone.ToString();
+            if (stoneStr.Length % 2 == 0)
+            {
+                int halfStr = stoneStr.Length / 2;
+                long result =
+                    CountStones(long.Parse(stoneStr.Substring(0, halfStr)), depth + 1, maxDepth)
+                    + CountStones(
+                        long.Parse(stoneStr.Substring(halfStr, halfStr)),
+                        depth + 1,
+                        maxDepth
+                    );
+                CountCache.Add((stone, depth), result);
+                return result;
+            }
+            else
+            {
+                long result = CountStones(stone * 2024, depth + 1, maxDepth);
+                CountCache.Add((stone, depth), result);
+                return result;
+            }
+        }
     }
 }
--- README.md
+++ README.md
@@ -2,13 +2,16 @@
 Advent of Code 2024 in C#.
 
 ## Usage
-Run it in Visual Studio?
+```dotnet run```
 
 ## Completion Calendar ⭐
 | Mon | Tue | Wed | Thu | Fri | Sat | Sun |
 |:---:|:---:|:---:|:---:|:---:|:---:|:---:|
 |     |     |     |     |     |     | Day 01 <br> ⭐⭐ |
 | Day 02 <br> ⭐⭐ | Day 03 <br> ⭐⭐ | Day 04 <br> ⭐⭐ | Day 05 <br> ⭐⭐ | Day 06 <br> ⭐⭐ | Day 07 <br> ⭐⭐ | Day 08 <br> ⭐⭐ |
-| Day 09 <br> ⭐⭐ | Day 10 <br> ⭐⭐ | Day 11 <br> ❌❌ | Day 12 <br> ❌❌ | Day 13 <br> ❌❌ | Day 14 <br> ❌❌ | Day 15 <br> ❌❌ |
+| Day 09 <br> ⭐⭐ | Day 10 <br> ⭐⭐ | Day 11 <br> ⭐⭐ | Day 12 <br> ❌❌ | Day 13 <br> ❌❌ | Day 14 <br> ❌❌ | Day 15 <br> ❌❌ |
 | Day 16 <br> ❌❌ | Day 17 <br> ❌❌ | Day 18 <br> ❌❌ | Day 19 <br> ❌❌ | Day 20 <br> ❌❌ | Day 21 <br> ❌❌ | Day 22 <br> ❌❌ |
 | Day 23 <br> ❌❌ | Day 24 <br> ❌❌ | Day 25 <br> ❌❌ |
+
+## Generating files
+Compile the generator with `csc GenerateFiles.cs`. The program takes two arguments, the directory to write to and the day to start from. By default this is the current directory and day 1.
